9d2cfb174c6c447c41ff625eb3c83e77255c5b52
Renee Matthews <reneeymatthews@gmail.com>
2021-07-01 14:00:22 -0400

    Initial Commit

---
+++ src/components/About.js
@@ -0,0 +1,16 @@
+import React, {Component} from 'react';
+
+class About extends Component{
+    constructor(props){
+        super(props);
+    }
+    render(){
+        return(
+            <div>
+
+            </div>
+        )
+    }
+}
+
+export default About;
---
+++ src/components/Main.js
@@ -0,0 +1,22 @@
+import { render } from '@testing-library/react';
+import React, {Component} from 'react';
+import Header from './Header';
+import Footer from './Footer';
+import Home from './Home';
+import Contact from './Contact';
+import About from './About';
+
+class Main extends Component{
+    constructor(props){
+        super(props);
+    }
+    render(){
+        return(
+            <div>
+
+            </div>
+        )
+    }
+}
+
+export default Main;
